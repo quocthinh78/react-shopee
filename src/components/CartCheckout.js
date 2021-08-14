2d3d152387e041e8b325ccf2f9f54d38c3d3394c
quocthinh78 <thinhvqps11484@fpt.edu.vn>
2021-08-14 13:48:46 +0700

    fwd

--- src/components/CartCheckout.js
+++ src/components/CartCheckout.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { IconName ,FcCurrencyExchange } from "react-icons/fc";
 import { RiErrorWarningLine } from "react-icons/ri"
-import { FcCurrencyExchange } from "react-icons/fc"
 import { currencyFormat } from '../common/currency';
 export default function CartCheckout(props) {
     let { cartProduct, typeMoney } = props;
